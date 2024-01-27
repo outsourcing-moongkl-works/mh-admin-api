499ce218d54e952e322b1a1ed07d54dd5e639f3b
Subin <70842920+bin-pro@users.noreply.github.com>
2024-01-27 13:46:34 +0900

    Init: 2nd Outsourcing Project - moongkl here admin

---
+++ src/main/java/org/outsourcing/mhadminapi/MhAdminApiApplication.java
@@ -0,0 +1,13 @@
+package org.outsourcing.mhadminapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MhAdminApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MhAdminApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/outsourcing/mhadminapi/MhAdminApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.outsourcing.mhadminapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MhAdminApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
