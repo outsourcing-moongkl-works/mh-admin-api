01a23c3f6b51f56546fe6c0f12a738e07ffe3e22
Subin <70842920+bin-pro@users.noreply.github.com>
2024-03-04 22:56:22 +0900

    Chore: test

--- src/main/java/org/outsourcing/mhadminapi/MhAdminApiApplication.java
+++ src/main/java/org/outsourcing/mhadminapi/MhAdminApiApplication.java
@@ -9,7 +9,7 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
 @SpringBootApplication
 @EnableJpaAuditing
 public class MhAdminApiApplication {
-
+	//test
 	public static void main(String[] args) {
 		SpringApplication.run(MhAdminApiApplication.class, args);
 
