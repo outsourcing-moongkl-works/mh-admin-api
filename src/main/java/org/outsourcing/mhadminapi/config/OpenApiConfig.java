8d677b01d61bea946cc82a80be394bc2c059da48
Subin <70842920+bin-pro@users.noreply.github.com>
2024-03-04 23:30:32 +0900

    Chore: Fix swagger config

--- src/main/java/org/outsourcing/mhadminapi/config/OpenApiConfig.java
+++ src/main/java/org/outsourcing/mhadminapi/config/OpenApiConfig.java
@@ -8,10 +8,12 @@ import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
@@ -27,7 +29,12 @@ public class OpenApiConfig {
                         .description("어드민 페이지 API LIST")
                         .version("v1.0"))
                         .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
-                        .security(Arrays.asList(securityRequirement));
+                        .security(Arrays.asList(securityRequirement))
+                        .servers(Arrays.asList(
+                        new Server().url("https://admin-api.moongkl.com").description("Production server"),
+                        new Server().url("http://localhost:8080").description("Local development server"),
+                        new Server().url("http://admin-api.moongkl.com").description("HTTP Production server")
+                        ));
                         /*.contact(new Contact()
                                 .name("Contact Name")
                                 .url("http://your-contact-url.com")
--- src/main/java/org/outsourcing/mhadminapi/config/SecurityConfig.java
+++ src/main/java/org/outsourcing/mhadminapi/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package org.outsourcing.mhadminapi.config;
 
 import lombok.RequiredArgsConstructor;
+import org.outsourcing.mhadminapi.auth.JwtTokenFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -13,18 +14,24 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.server.util.matcher.NegatedServerWebExchangeMatcher;
 import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @RequiredArgsConstructor
 public class SecurityConfig{
-    private final UserDetailsService userDetailsService;
+    private final JwtTokenFilter jwtTokenFilter;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http    .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.disable())
                 .authorizeHttpRequests(authz -> authz
                         .anyRequest().permitAll())
                 .sessionManagement(session -> session
@@ -33,6 +40,8 @@ public class SecurityConfig{
                         .invalidateHttpSession(true)
                         .deleteCookies("JSESSIONID"))
                 ;
+
+        http.addFilterAfter(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
@@ -43,5 +52,17 @@ public class SecurityConfig{
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.addAllowedOrigin("http://localhost:3000");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
