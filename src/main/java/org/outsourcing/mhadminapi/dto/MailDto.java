b94679da7c97d697b8645d6cf88b0187c2c1693e
Subin <70842920+bin-pro@users.noreply.github.com>
2024-03-20 15:21:48 +0900

    Fix: error for mail

---
+++ src/main/java/org/outsourcing/mhadminapi/dto/MailDto.java
@@ -0,0 +1,22 @@
+package org.outsourcing.mhadminapi.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class MailDto {
+
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+    public static class MailSendDto{
+        private String to;
+        private String title;
+        private String content;
+    }
+}
---
+++ src/main/java/org/outsourcing/mhadminapi/service/MailService.java
@@ -0,0 +1,32 @@
+package org.outsourcing.mhadminapi.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.outsourcing.mhadminapi.dto.MailDto;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service @Slf4j
+@RequiredArgsConstructor
+public class MailService {
+
+    private final JavaMailSender mailSender;
+
+    @Value("${spring.mail.username}")
+    private String FROM_ADDRESS;
+
+    public void sendMail(MailDto.MailSendDto mailSendDto) {
+
+        String email = mailSendDto.getTo();
+
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(email);
+        mailMessage.setFrom(FROM_ADDRESS);
+        mailMessage.setSubject(mailSendDto.getTitle());
+        mailMessage.setText(mailSendDto.getContent());
+
+        mailSender.send(mailMessage);
+    }
+}
