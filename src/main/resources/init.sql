abd8c5f2adb1e304805e95fdf2e6ea175bd81f6f
Subin <70842920+bin-pro@users.noreply.github.com>
2024-03-21 23:24:29 +0900

    Init: add test user

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -1,3 +1,12 @@
+-- Users 테이블 생성
+CREATE TABLE IF NOT EXISTS terms
+(
+    id BINARY(16) NOT NULL PRIMARY KEY,
+    terms VARCHAR(10000),
+    created_at DATETIME,
+    updated_at DATETIME
+    );
+
 -- Terms 테이블 생성
 CREATE TABLE IF NOT EXISTS terms
 (
@@ -33,6 +42,22 @@ CREATE TABLE IF NOT EXISTS company_location
     created_at DATETIME,
     updated_at DATETIME
     );
+
+-- Admin 테이블 생성
+CREATE TABLE IF NOT EXISTS `admins` (
+                                        `id` BINARY(16) NOT NULL,
+    `admin_email` VARCHAR(50) NOT NULL,
+    `password` VARCHAR(100) NOT NULL,
+    `role` VARCHAR(20) NOT NULL,
+    `created_at` DATETIME(6) NULL,
+    `updated_at` DATETIME(6) NULL,
+    PRIMARY KEY (`id`)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- MASTER role을 가진 테스트 유저 생성
+INSERT INTO `admins` (`id`, `admin_email`, `password`, `role`, `created_at`, `updated_at`)
+VALUES
+    (UUID_TO_BIN(UUID()), 'test@admin.com', 'testpassword', 'MASTER', NOW(), NOW());
 
 -- Terms 테이블에 조건부 삽입
 INSERT IGNORE INTO terms (id) VALUES (UNHEX(REPLACE('4000c0f7-0c97-4bd7-a200-0de1392f1df0', '-', '')));
