e4f16190ebfd0d8cc43c76da287621e7533572da
Subin <70842920+bin-pro@users.noreply.github.com>
2024-04-14 15:35:00 +0900

    chore: uuid

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
 
 -- MASTER role을 가진 테스트 유저 생성
 INSERT INTO `admins` (id, email, password, role, created_at, updated_at)
-VALUES (UNHEX(REPLACE(UUID(), '-', '')), 'test@admin.com', '$2a$12$./wHNApeq/feNKojcfkWouvZJfPrhdbDKWXQZWZy9PaW7pnkt/ZV6', 'MASTER', NOW(), NOW())
+VALUES (UNHEX(REPLACE('4012c0f7-0c97-4bd7-a200-0de1392f1df0', '-', '')), 'test@admin.com', '$2a$12$./wHNApeq/feNKojcfkWouvZJfPrhdbDKWXQZWZy9PaW7pnkt/ZV6', 'MASTER', NOW(), NOW())
 ON DUPLICATE KEY UPDATE email = email;
 
 -- Terms 테이블에 조건부 삽입
@@ -18,5 +18,5 @@ INSERT IGNORE INTO company_location (id) VALUES (UNHEX(REPLACE('7ddde530-4d8a-42
 
 -- enterprises 테이블에 테스트 계정 삽입
 INSERT INTO enterprises (id, login_id, password, role, name, business_number, corporate_number, address, is_approved, manager_name, manager_phone, manager_email, country, created_at, updated_at)
-VALUES (UNHEX(REPLACE(UUID(), '-', '')), 'testid', '$2a$12$./wHNApeq/feNKojcfkWouvZJfPrhdbDKWXQZWZy9PaW7pnkt/ZV6', 'ENTERPRISE', 'Test Enterprise', '123-45-6789', '987-65-4321', 'Test Address', 1, 'Manager Name', '010-1234-5678', 'manager@test.com', 'korea', NOW(), NOW())
+VALUES (UNHEX(REPLACE('4013c0f7-0c97-4bd7-a200-0de1392f1df0', '-', '')), 'testid', '$2a$12$./wHNApeq/feNKojcfkWouvZJfPrhdbDKWXQZWZy9PaW7pnkt/ZV6', 'ENTERPRISE', 'Test Enterprise', '123-45-6789', '987-65-4321', 'Test Address', 1, 'Manager Name', '010-1234-5678', 'manager@test.com', 'korea', NOW(), NOW())
 ON DUPLICATE KEY UPDATE login_id = login_id;
