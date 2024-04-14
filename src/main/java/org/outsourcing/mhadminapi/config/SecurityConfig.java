f40590962b8d7a27963a0edf03f3f24a6d88737a
Subin <70842920+bin-pro@users.noreply.github.com>
2024-04-14 15:51:44 +0900

    Chore: add allow origin for cors

--- src/main/java/org/outsourcing/mhadminapi/config/SecurityConfig.java
+++ src/main/java/org/outsourcing/mhadminapi/config/SecurityConfig.java
@@ -21,6 +21,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
 import java.util.Collections;
 
 @Configuration
@@ -81,7 +82,7 @@ public class SecurityConfig{
             CorsConfiguration config = new CorsConfiguration();
             config.setAllowedHeaders(Collections.singletonList("*"));
             config.setAllowedMethods(Collections.singletonList("*"));
-            config.setAllowedOriginPatterns(Collections.singletonList("http://localhost:3000")); // 허용할 origin
+            config.setAllowedOriginPatterns(Arrays.asList("http://localhost:3000", "https://admin-api.moongkl.com"));
             config.setAllowCredentials(true);
             return config;
         };
