18e2c3d4eb7411d0e0796e78bf1f942083e46d1d
Subin <70842920+bin-pro@users.noreply.github.com>
2024-05-15 14:24:27 +0900

    Chore: add test user

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -20,3 +20,26 @@ INSERT IGNORE INTO company_location (id) VALUES (UNHEX(REPLACE('7ddde530-4d8a-42
 INSERT INTO enterprises (id, login_id, password, role, name, business_number, corporate_number, address, is_approved, manager_name, manager_phone, manager_email, country, created_at, updated_at)
 VALUES (UNHEX(REPLACE('4013c0f7-0c97-4bd7-a200-0de1392f1df0', '-', '')), 'testid', '$2a$12$./wHNApeq/feNKojcfkWouvZJfPrhdbDKWXQZWZy9PaW7pnkt/ZV6', 'ENTERPRISE', 'Test Enterprise', '123-45-6789', '987-65-4321', 'Test Address', 1, 'Manager Name', '010-1234-5678', 'manager@test.com', 'korea', NOW(), NOW())
 ON DUPLICATE KEY UPDATE login_id = login_id;
+
+-- users 테이블에 테스트 계정 삽입
+/*
+     @Builder(toBuilder = true)
+    public User(UUID id,
+                String email,
+                String password,
+                String phoneNumber,
+                String gender,
+                String country,
+                LocalDateTime createdAt) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.gender = gender;
+        this.country = country;
+        this.phoneNumber = phoneNumber;
+        this.createdAt = createdAt;
+ */
+
+INSERT INTO users (id, email, password, phone_number, gender, country, created_at)
+VALUES (UNHEX(REPLACE('4014c0f7-0c97-4bd7-a200-0de1392f1df0', '-', '')), 'test@user.com', 'Test123456!', '01012341234', 'male', '대한민국', NOW())
+ON DUPLICATE KEY UPDATE email = email;
